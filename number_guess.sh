04e0ae65991d2a8eb335ccfc74a15f859e68f951
dennis61820 <danngit@hotmail.com>
2024-04-01 16:27:55 +0000

    Initial commit

---
+++ number_guess.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# generate a number between 1 and 1000
+
+# prompt for user name
+
+# if first time welcome and mention first time
+
+# if not welcome and give play history
+
+# prompt to guess number
+
+# if not integet prompt again
+
+# if lower say lower
+
+# prompt again
+
+# if higher say higher
+
+# prompt again
+
+# on success say how many tries it took
+
+
