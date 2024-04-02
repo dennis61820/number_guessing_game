38abb807eec4d23dd2cb94c8838f276cb506957e
dennis61820 <danngit@hotmail.com>
2024-04-02 15:01:45 +0000

    refactor: number test

--- number_guess.sh
+++ number_guess.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
 
 
-# generate a number between 1 and 1000
+# # generate a number between 1 and 1000
 NUMBER=$(($RANDOM % 1000 + 1))
 echo $NUMBER
+  echo -e "\nEnter your username:"
+read USER_NAME
+
+  echo -e "\nGuess the secret number between 1 and 1000:"
+
+
+
+GUESS_NUM () {
+  #echo -e "\nEnter your username:"
+  
+  read GUESS 
+
+  # 1 number correct
+    if [[ $GUESS == $NUMBER ]]
+    then
+    echo -e "\nYou guessed it in <number_of_guesses> tries. The secret number was $NUMBER. Nice job!" 
+  # 2 number low
+    elif [[ $GUESS > $NUMBER ]]
+    then
+   echo -e "\nIt's lower than that, guess again:"
+   GUESS_NUM
+  # 3 number high 
+    elif [[ $GUESS < $NUMBER ]]
+    then
+    echo -e "\nIt's higher than that, guess again:"
+    GUESS_NUM
+  # 4 else
+    else
+    echo -e "\nThat is not an integer, guess again:"
+  GUESS_NUM 
+  fi
+  
+}
+
 
 # prompt for user name
-read USER_NAME
-echo $USER_NAME
+GUESS_NUM
 
 # if first time welcome and mention first time
 
@@ -15,16 +48,21 @@ echo $USER_NAME
 
 # prompt to guess number
 
+
 # if not integet prompt again
 
+
 # if lower say lower
+
 
 # prompt again
 
 # if higher say higher
 
+
 # prompt again
 
 # on success say how many tries it took
+
 
 
